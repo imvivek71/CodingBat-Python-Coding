8f7d878d5a2e4a567c39c3a1879772932c89dcec
Vivek Goswami <38568313+imvivek71@users.noreply.github.com>
2019-01-22 18:35:36 +0530

    Create reverse3.py

---
+++ List_1/reverse3.py
@@ -0,0 +1,15 @@
+"""""
+Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+
+
+reverse3([1, 2, 3]) → [3, 2, 1]
+reverse3([5, 11, 9]) → [9, 11, 5]
+reverse3([7, 0, 0]) → [0, 0, 7]
+
+"""
+def reverse3(nums):
+  list = []
+  for i in range(3):
+    list.append(nums[2-i])
+  return list
+    
