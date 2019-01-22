538c33e0920a15d09d54cadf9f7ee8c39b239fbf
Vivek Goswami <38568313+imvivek71@users.noreply.github.com>
2019-01-22 23:25:44 +0530

    Create end_other.py

---
+++ string_2/end_other.py
@@ -0,0 +1,17 @@
+"""""
+Given two strings, return True if either of the strings appears at the very end of the other string, 
+ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). 
+Note: s.lower() returns the lowercase version of a string.
+
+
+end_other('Hiabc', 'abc') → True
+end_other('AbC', 'HiaBc') → True
+end_other('abc', 'abXabc') → True
+
+"""
+def end_other(a, b):
+  a = a.lower()
+  b = b.lower()
+  if a.endswith(b) or b.endswith(a):
+    return True
+  return False
