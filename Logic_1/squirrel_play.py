aac10e08d492f283f1f42becc2f4059b4bb65834
Vivek Goswami <38568313+imvivek71@users.noreply.github.com>
2019-01-24 00:18:31 +0530

    Create squirrel_play.py

---
+++ Logic_1/squirrel_play.py
@@ -0,0 +1,16 @@
+"""""
+The squirrels in Palo Alto spend most of the day playing. In particular, 
+they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, 
+then the upper limit is 100 instead of 90. Given an int temperature and a boolean is_summer,
+return True if the squirrels play and False otherwise.
+
+
+squirrel_play(70, False) → True
+squirrel_play(95, False) → False
+squirrel_play(95, True) → True
+ 
+"""
+def squirrel_play(temp, is_summer):
+  if (temp in range(60,91)) or( is_summer==1 and temp in range(60,101) ):
+    return True
+  return False
